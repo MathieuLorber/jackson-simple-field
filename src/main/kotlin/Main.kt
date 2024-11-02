3aa8353c46683126ae45f6cb8f7a87584a5bdc64
Mathieu Lorber <mathieu.lorber@gmail.com>
2024-11-02 17:22:18 +0100

    Serialization ok

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+
+data class SimpleDataClass(val username: String)
+
 fun main() {
-    println("Hello World!")
+    val objectMapper = ObjectMapper().apply {
+        registerKotlinModule()
+    }
+
+    val json = objectMapper.writeValueAsString(SimpleDataClass("test"))
+    require(json == """{"username":"test"}""")
+    objectMapper.readValue(json, SimpleDataClass::class.java)
 }
